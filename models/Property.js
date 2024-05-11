8e76948e5afe4c56f7214ec7bc075ab1d2686c69
dmacisso <dmacisso@gmail.com>
2024-05-11 13:36:32 -0400

    create user and property models

---
+++ models/Property.js
@@ -0,0 +1,40 @@
+import { Schema, model, models } from 'mongoose';
+
+const propertySchema = new Schema(
+  {
+    owner: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+    name: { type: String, required: [true, 'Name is required'] },
+    type: { type: String, required: [true, 'Type is required'] },
+    location: {
+      street: { type: String },
+      city: { type: String },
+      state: { type: String },
+      zipcode: { type: String },
+    },
+    beds: { type: Number, required: [true, 'Beds is required'] },
+    baths: { type: Number, required: [true, 'Baths is required'] },
+    square_feet: { type: Number, required: [true, 'Square feet is required'] },
+    amenities: [{ type: String }],
+    rates: {
+      nightly: { type: Number },
+      weekly: { type: Number },
+      monthly: { type: Number },
+    },
+    seller_info: {
+      name: { type: String },
+      email: { type: String },
+      phone: { type: String },
+    },
+    images: [
+      {
+        type: String,
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Property = models.Property || model('Property', propertySchema);
+export default Property;
