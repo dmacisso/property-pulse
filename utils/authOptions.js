ef7ecbe268d1c4f5226c64ac93dc4905fb5421e0
dmacisso <dmacisso@gmail.com>
2024-05-17 11:54:42 -0400

    Begin work on auth provider

---
+++ utils/authOptions.js
@@ -0,0 +1,32 @@
+import GoogleProvider from 'next-auth/providers/google';
+
+export const authOptions = {
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      authorization: {
+        params: {
+          prompt: 'consent',
+          access_type: 'offline',
+          response_type: 'code',
+        },
+      },
+    }),
+  ],
+  callbacks: {
+    // Invoked on successful authentication
+    async signIn({ profile }) {
+      // 1. Connect to the database
+      // 2. See if the user already exists
+      // 3. If not, create a new user in the database
+      // 4. Return true to signify that we've created the user
+    },
+    // Modifies the session object
+    async session({ session }) {
+      // 1. Get the currently logged in user from the database
+      // 2. Assign the user id to the session
+      // 3.return session;
+    },  
+  },
+};
