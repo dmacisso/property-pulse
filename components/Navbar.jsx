e4521f6b72c4aa21355b553b1ad3bac4e43fdf20
dmacisso <dmacisso@gmail.com>
2024-05-19 14:02:23 -0400

    Finish Sign Out and protect routes with middleware

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -12,6 +12,7 @@ import { signIn, signOut, useSession, getProviders } from 'next-auth/react';
 
 const Navbar = () => {
   const { data: session } = useSession();
+  const profileImage = session?.user?.image;
   // MARK: STATE
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
   const [isProfileMenuOpen, setIsProfileMenuOpen] = useState(false);
@@ -28,7 +29,7 @@ const Navbar = () => {
     setAuthProviders();
   }, []);
 
-  // console.log(session);
+  // console.log(profileImage);
 
   return (
     <nav className="bg-blue-700 border-b border-blue-500">
@@ -169,8 +170,10 @@ const Navbar = () => {
                     <span className="sr-only">Open user menu</span>
                     <Image
                       className="h-8 w-8 rounded-full"
-                      src={profileDefault}
+                      src={profileImage || profileDefault}
                       alt=""
+                      width={40}
+                      height={40}
                     />
                   </button>
                 </div>
@@ -191,6 +194,7 @@ const Navbar = () => {
                       role="menuitem"
                       tabIndex="-1"
                       id="user-menu-item-0"
+                      onClick={() => setIsProfileMenuOpen(false)}
                     >
                       Your Profile
                     </Link>
@@ -200,10 +204,15 @@ const Navbar = () => {
                       role="menuitem"
                       tabIndex="-1"
                       id="user-menu-item-2"
+                      onClick={() => setIsProfileMenuOpen(false)}
                     >
                       Saved Properties
                     </Link>
                     <button
+                      onClick={() => {
+                        setIsProfileMenuOpen(false);
+                        signOut();
+                      }}
                       className="block px-4 py-2 text-sm text-gray-700"
                       role="menuitem"
                       tabIndex="-1"
---
+++ middleware.js
@@ -0,0 +1,11 @@
+export { default } from 'next-auth/middleware';
+
+export const config = {
+  matcher: [
+    '/properties/add',
+    '/profile',
+    '/messages',
+    '/api/properties/saved',
+  ],
+};
+
